a04a6b9246988c7e481fdb0cde2c37c3d2eec9bf
Satish Goda <satishgoda@live.com>
2017-01-19 11:31:26 +0800

    Create ipy.py
    https://projecteuler.net/problem=1

---
+++ math/numbers/sandbox/ipy.py
@@ -0,0 +1,33 @@
+(100-1)/5
+(100-1)/3
+100/3
+mul3 = map(lambda x: 3*x, range(1, 33+1))
+mul3
+mul5 = map(lambda x: 5*x, range(1, 1+1))
+mul5
+mul5 = map(lambda x: 5*x, range(1, 19+1))
+mul5
+sum
+sum?
+sum(mul3, mul5)
+sum([mul3, mul5])
+mul3
+sum(mul3)
+sum(mul3)+sum(mul5)
+mul3
+mul5
+sum(set(mul5).union(set(mul3)))
+set(mul5).union(set(mul3))
+sum(list(set(mul5).union(set(mul3))))
+set(mul5).intersection(set(mul3))
+sum(set(mul5).intersection(set(mul3)))
+sum(list(set(mul5).union(set(mul3))))
+2318-315
+set(mul3) - set(mul5)
+set(mul5) - set(mul3)
+sum(set(mul5) - set(mul3))
+sum(set(mul3) - set(mul5))
+1368+635
+set(mul5) ^ set(mul3)
+sum(set(mul5) ^ set(mul3))
+history
