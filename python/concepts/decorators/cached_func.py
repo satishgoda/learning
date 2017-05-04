9511dea11b10adef776e39fdebc980cfee9c798d
Satish Goda <satishgoda@live.com>
2017-05-04 14:16:59 +0800

    Update cached_func.py

--- python/concepts/decorators/cached_func.py
+++ python/concepts/decorators/cached_func.py
@@ -27,9 +27,18 @@ def add2(x, y):
     """
     return x + y
 
-print add2(2, 3)
+print add2(2, 3) # value is computed and cached
+print add2(3, 2) # value is computed and cached
+print add2(2, 3) # value is returned from the cache
 
-print add2(3, 2)
+>>> add2.func_closure
+(<cell at 0x24994b0: function object at 0x23ef398>,)
 
-print add2(2, 3)
+>>> add2.func_closure[0].cell_contents
+<function add2 at 0x23ef398>
 
+>>> add2.func_closure[0].cell_contents.__dict__
+{((3, 2), ()): 5, ((2, 3), ()): 5}
+
+>>> inspect.isfunction(add2.func_closure[0].cell_contents)
+True
